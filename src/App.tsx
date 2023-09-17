06279192ae8f0150e106167a30029e71861b0ad1
LuisKrotz <luis.krotz@gmail.com>
2023-09-17 19:10:09 -0300

    setup(kickoff): test lunr.js in react + firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+// Import React and lunr
 
-function App() {
-  const [count, setCount] = useState(0)
+// DB
+// https://firebase.google.com/docs/database/web/read-and-write
+import firebaseConfig from './app/firebaseConfig.ts'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+import { child, get, getDatabase, ref } from 'firebase/database'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import React, { useState, useEffect } from 'react'
+import lunr from 'lunr'
+
+// Define the interface for the document type
+interface Document {
+    id: string
+    title: string
+    body: string
 }
 
-export default App
+// Define some sample documents
+const documents: Document[] = [
+    {
+        id: '1',
+        title: 'Hello World',
+        body: 'This is a simple document that says hello to the world.'
+    },
+    {
+        id: '2',
+        title: 'Goodbye World',
+        body: 'This is another document that says goodbye to the world.'
+    }
+]
+
+// Define a component that uses lunr to search the documents
+const SearchComponent: React.FC = () => {
+    // Initialize the state variables
+    const [query, setQuery] = useState('')
+    const [results, setResults] = useState<Document[]>([])
+    const [index, setIndex] = useState<lunr.Index | null>(null)
+
+    // Initialize Firebase
+    const app  = initializeApp(firebaseConfig)
+    const analytics = getAnalytics(app )
+    const dbRef = ref(getDatabase())
+
+    const getDocuments = () => {
+        get(child(dbRef, 'exchange'))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const data = snapshot.val()
+
+                    console.log('available data', data)
+                } else {
+                    console.log('No data available')
+                }
+            })
+            .catch((error) => console.error(error))
+    }
+
+    // Create the lunr index when the component mounts
+    useEffect(() => {
+        getDocuments()
+
+        setIndex(
+            lunr(function () {
+                this.ref('id')
+                this.field('title')
+                this.field('body')
+
+                documents.forEach((doc) => {
+                    this.add(doc)
+                })
+            })
+        )
+    }, [])
+
+    // Handle the query change event
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        // Get the query value
+        const query = event.target.value
+
+        // Set the query state
+        setQuery(query)
+
+        // Search the index with the query
+        if (index) {
+            const results = index.search(query).map(({ ref }) => {
+                return documents.find((doc) => doc.id === ref)
+            }) as Document[]
+
+            // Set the results state
+            setResults(results)
+        }
+    }
+
+    // Render the component
+    return (
+        <div>
+            <h1>Search Component</h1>
+            <input type="text" value={query} onChange={handleChange} />
+            <ul>
+                {results.map((doc) => (
+                    <li key={doc.id}>
+                        <h2>{doc.title}</h2>
+                        <p>{doc.body}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default SearchComponent
