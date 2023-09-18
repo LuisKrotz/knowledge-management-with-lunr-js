837d469beabddc010ec251086daf81442b209ae0
LuisKrotz <luis.krotz@gmail.com>
2023-09-17 21:53:52 -0300

    feat(general): create search capability with lunr + firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-// Import React and lunr
 
-// DB
-// https://firebase.google.com/docs/database/web/read-and-write
-import firebaseConfig from './app/firebaseConfig.ts'
+import firebaseConfig from './app/firebaseConfig'
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { child, get, getDatabase, ref } from 'firebase/database'
@@ -12,102 +9,128 @@ import lunr from 'lunr'
 
 // Define the interface for the document type
 interface Document {
-    id: string
-    title: string
-    body: string
+  id: string
+  title: string
+  body: string
 }
-
-// Define some sample documents
-const documents: Document[] = [
-    {
-        id: '1',
-        title: 'Hello World',
-        body: 'This is a simple document that says hello to the world.'
-    },
-    {
-        id: '2',
-        title: 'Goodbye World',
-        body: 'This is another document that says goodbye to the world.'
-    }
-]
 
 // Define a component that uses lunr to search the documents
 const SearchComponent: React.FC = () => {
-    // Initialize the state variables
-    const [query, setQuery] = useState('')
-    const [results, setResults] = useState<Document[]>([])
-    const [index, setIndex] = useState<lunr.Index | null>(null)
+  // Initialize the state variables
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState<Document[]>([])
+  const [index, setIndex] = useState<lunr.Index | null>(null)
+  const [documents, setDocuments] = useState<Document[]>([])
+  const [loading, setLoading] = useState(true)
 
-    // Initialize Firebase
-    const app  = initializeApp(firebaseConfig)
-    const analytics = getAnalytics(app )
-    const dbRef = ref(getDatabase())
+  // Initialize Firebase
+  const firebase = initializeApp(firebaseConfig)
+  const analytics = getAnalytics(firebase)
+  const dbRef = ref(getDatabase())
 
-    const getDocuments = () => {
-        get(child(dbRef, 'documents'))
-            .then((snapshot) => {
-                if (snapshot.exists()) {
-                    const data = snapshot.val()
+  const getDocuments = (): Promise<Document[]> => {
+    return get(child(dbRef, 'documents/en-us'))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          const data = snapshot.val()
+          const entries: Document[] = []
 
-                    console.log('available data', data)
-                } else {
-                    console.log('No data available')
-                }
+          data.forEach((entry, index) => {
+            entries.push({
+              id: index,
+              title: entry.title,
+              body: entry.body
             })
-            .catch((error) => console.error(error))
-    }
+          })
 
-    // Create the lunr index when the component mounts
-    useEffect(() => {
-        getDocuments()
+          return Object.values(entries)
 
-        setIndex(
-            lunr(function () {
-                this.ref('id')
-                this.field('title')
-                this.field('body')
+        } else {
+          console.log('No data available')
 
-                documents.forEach((doc) => {
-                    this.add(doc)
-                })
-            })
-        )
-    }, [])
-
-    // Handle the query change event
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        // Get the query value
-        const query = event.target.value
-
-        // Set the query state
-        setQuery(query)
-
-        // Search the index with the query
-        if (index) {
-            const results = index.search(query).map(({ ref }) => {
-                return documents.find((doc) => doc.id === ref)
-            }) as Document[]
-
-            // Set the results state
-            setResults(results)
+          return []
         }
+      })
+      .catch((error) => {
+        console.error(error)
+        return []
+      })
+  }
+
+  // Create the lunr index when the component mounts
+  useEffect(() => {
+    // Define an async function to fetch and index the documents
+    const fetchAndIndex = async () => {
+      // Wait for the promise to resolve and assign it to documents
+      const docs: Document[] = await getDocuments()
+
+      // Update the documents state
+      setDocuments(docs)
+
+      setIndex(
+        lunr(function () {
+          this.ref('id')
+          this.field('title')
+          this.field('body')
+
+          docs.forEach((doc) => {
+            this.add(doc)
+          })
+        })
+      )
+
+      // Set the loading state to false
+      setLoading(false)
     }
 
-    // Render the component
-    return (
-        <div>
-            <h1>Search Component</h1>
-            <input type="text" value={query} onChange={handleChange} />
-            <ul>
-                {results.map((doc) => (
-                    <li key={doc.id}>
-                        <h2>{doc.title}</h2>
-                        <p>{doc.body}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+    // Call the async function
+    fetchAndIndex()
+  }, [])
+
+  // Handle the query change event
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Get the query value
+    const query = event.target.value
+
+    // Set the query state
+    setQuery(query)
+
+    console.log(query, results, documents)
+
+    // Search the index with the query
+    if (index) {
+      const results = index.search(query).map(({ ref }) => {
+        // Convert both ref and id to numbers before comparing them
+        return documents.find((doc) => Number(doc.id) === Number(ref))
+      }) as Document[]
+
+
+      console.log(results)
+
+      // Set the results state
+      setResults(results)
+    }
+  }
+
+  // Render the component
+  return (
+    <div>
+      <h1>Search Component</h1>
+      <input type='text' value={query} onChange={handleChange} />
+      <ul>
+        {loading ? (
+          <li>Loading...</li>
+        ) : (
+          results.map((doc) => (
+            <li key={doc.id}>
+              <h2>{doc.title}</h2>
+              <p>{doc.body}</p>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default SearchComponent
